57db8e42e78d9fb3d675249bd9c40aa5d8da225f
Vitaliy Gromov <gromov.vitaliy03@gmail.com>
2023-05-11 15:23:51 +0300

    add fields into create artist view

--- resources/views/ads/artist/create.blade.php
+++ resources/views/ads/artist/create.blade.php
@@ -39,8 +39,22 @@
           <div class="mt-3">
             <h3>{{__('Дополнительная информация')}}</h3>
           </div>
+          <div class="mt-3">
+            <x-checkbox name="own_music">{{(__('Свой материал'))}}</x-checkbox>
+          </div>
+          <div class="mt-3">
+            <x-checkbox name="cower_band">{{(__('Кавер-группа'))}}</x-checkbox>
+          </div>
+          <div class="mt-3">
+            <x-checkbox name="commercial_project">{{(__('Коммерческий проект'))}}</x-checkbox>
+          </div>
+          <div class="mt-3">
+            <label class="form-check-label">{{__('Сколько хотите деняк?)')}}</label>
+            <input type="number" name="salary" class="form-control"/>
+          </div>
           <button type="submit" class="btn btn-primary mt-3">{{__('Создать')}}</button>
         </form>
     </div>
 </div>
 @endsection
+<script></script>
