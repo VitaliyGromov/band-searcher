3394287ae97bd7b4944f59f0ac5730a3d1b1821b
Vitaliy Gromov <gromov.vitaliy03@gmail.com>
2023-07-07 22:13:21 +0300

    addd new methods to ModelFilters/AdFilter

--- app/ModelFilters/AdFilter.php
+++ app/ModelFilters/AdFilter.php
@@ -21,4 +21,39 @@ class AdFilter extends ModelFilter
     {
         return $this->where('region_id', $id);
     }
+
+    public function city(int $id): static
+    {
+        return $this->where('city_id', $id);
+    }
+
+    public function applicantExperience(string $applicantExperience): static
+    {
+        return $this->where('applicant_experience', $applicantExperience);
+    }
+
+    public function applicantConcertExperience(string $applicantConcertExperience): static
+    {
+        return $this->where('applicant_concert_experience', $applicantConcertExperience);
+    }
+
+    public function skill(int $id): static
+    {
+        return $this->where('skill_id', $id);
+    }
+
+    public function genre(int $id): static
+    {
+        return $this->where('genre_id', $id);
+    }
+
+    public function bandName(string $bandName): static
+    {
+        return $this->where('band_name', $bandName);
+    }
+
+    public function salary(int $salary)
+    {
+        return $this->where('salary', $salary);
+    }
 }
